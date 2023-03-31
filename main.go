bca636142a680e3b736abe6a367d0c650c187168
dchf <dchf.jp@gmail.com>
2023-04-01 00:01:54 +0900

    x-www-form-urlencoded

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -9,20 +8,12 @@ import (
 
 func main() {
 	values := url.Values{
-		"query": {"hello world"},
+		"test": {"value"},
 	}
-	resp, err := http.Get("http://localhost:18888" + "?" + values.Encode())
+	resp, err := http.PostForm("http://localhost:18888", values)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(string(body))
 	log.Println("Status:", resp.Status)
-	log.Println("StatusCode:", resp.StatusCode)
-	log.Println("Header:", resp.Header)
-	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
 }
